68399ba1993321c60ea66fe06bbeb56ab1c7afae
Clayton Carlson <claytonrc97@gmail.com>
2023-01-10 21:34:19 -0800

    commit tree d6b6e847879ef1c1fe298bfb278963a33d3ae2ab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+//const names = ["Guadalupe", "Ollie", "Aki"];
+//const message = []
+function writeCards(names, message){
+    var newMessage = [];
+    for(let i = 0; i < names.length; i++){
+        newMessage.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)};
+
+return newMessage;
+    }
+
+
+
+function countDown(){
+let countDown = 10;
+while (countDown >= 0){
+    console.log(countDown);
+    countDown--;
+}
+}
+
